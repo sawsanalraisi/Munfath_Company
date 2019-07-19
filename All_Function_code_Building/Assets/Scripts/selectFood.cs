3f33e415fffda1cfddbcd86850d7964345c23799
sawsanalraisi <sawsanalraisi00@hotmail.com>
2019-07-19 23:56:46 +0400

    2

--- All_Function_code_Building/Assets/Scripts/selectFood.cs
+++ All_Function_code_Building/Assets/Scripts/selectFood.cs
@@ -13,53 +13,114 @@ public class selectFood : MonoBehaviour
     void Update()
     {
 
+        if(Input.GetMouseButtonDown(0))
+        {     
+       
+         RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit,150f))
+            {
+
+                // show spisific distance 
+
+                if (IsFood == false)
+                {
+
+                    if (hit.transform.name == "food1Sel")
+                    {
+                        food1.SetActive(true);
+                        food1.transform.position = new Vector3(-42.3f, 15.7f, 11.7f);
+                        IsFood = true;
+                        print("Have a good meal");
+                    }
+                    else if (hit.transform.name == "food2Sel")
+                    {
+                        food2.SetActive(true);
+                        food2.transform.position = new Vector3(-42.3f, 15.7f, 11.7f);
+                        IsFood = true;
+                        print("Have a good meal");
+                    }
+                    else if (hit.transform.name == "food3Sel")
+                    {
+                        food3.SetActive(true);
+                        food3.transform.position = new Vector3(-42.3f, 15.7f, 11.7f);
+                        IsFood = true;
+                        print("Have a good meal");
+                    }
+
+                }
+                else
+                {
+                    if (hit.transform.name == "food1")
+                    {
+                        food1.SetActive(false);
+                        IsFood = false;
+
+                    }
+                    else if (hit.transform.name == "food2")
+                    {
+                        food2.SetActive(false);
+                        IsFood = false;
+
+                    }
+                    else if (hit.transform.name == "food3")
+                    {
+                        food3.SetActive(false);
+                        IsFood = false;
+
+                    }
+
+
+                }
+
+            }
+
+        }
+    }
+}
+
+
+
+/*
+
+void EatFoodFun()
+{
+    if (Input.GetMouseButtonDown(0))
+    {
+
         RaycastHit hit;
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
         if (Physics.Raycast(ray, out hit))
         {
-            // show spisific distance 
-                if (hit.transform.name == "food1Sel")
-                {
-                  //  food1.SetActive(true);
-                   // IsFood = true;
-                    print("Have a good meal");
-                }
-                else if (hit.transform.name == "food2Sel")
-                {
-                    //food2.SetActive(true);
-                    //IsFood = true;
-                    print("Have a good meal");
-                }
-                else if (hit.transform.name == "food3Sel")
-                {
-                   // food3.SetActive(true);
-                    //IsFood = true;
-                    print("Have a good meal");
-                }
-              
-            }
-           /* else
+
+            if (IsFood == true)
             {
-                print("Take Your Foods");
 
                 if (hit.transform.name == "food1")
                 {
                     food1.SetActive(false);
+                    IsFood = false;
                 }
                 else if (hit.transform.name == "food2")
                 {
                     food2.SetActive(false);
+                    IsFood = false;
+
+
                 }
                 else if (hit.transform.name == "food2")
                 {
                     food3.SetActive(false);
+                    IsFood = false;
                 }
-
-            }*/
-
+            }
 
         }
-
     }
+}*/
+
+
+
 
